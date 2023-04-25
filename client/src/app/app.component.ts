b5755a10b6be0a5964dca961de063fb747fdbbc4
Yehan Yeshminda <yeshanyesh12@gmail.com>
2023-04-25 11:12:42 +0400

    created the angular apps, and make the navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http
+      .get<Pagination<Product[]>>('https://localhost:5001/api/products')
+      .subscribe({
+        next: (response) => (this.products = response.data),
+        error: (err) => console.log(err),
+      });
+  }
 }
